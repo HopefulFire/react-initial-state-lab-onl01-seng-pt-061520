69956a3f2806f564831047b2bd6e906611ff5146
Jade Lynn Masker <jadelynn@masker.family>
2021-02-20 19:51:31 -0500

    commit tree e26e33dd63a2719ff6e698ff667b52f751ab2ed9

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
-import React {Component} from "react"
+import React from "react"
+
+export default class ImageSlider extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			currentSlideIndex: 0
+		}
+	}
+
+	render() {
+		return (
+			<h1>I am on slide {this.state.currentSlideIndex}</h1>
+		)
+	}
+}
